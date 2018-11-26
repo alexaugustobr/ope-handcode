ce6a6f616ca8f46907a861667b872284e80b8707
henrique221 <hborges9294@gmail.com>
2018-11-26 02:21:38 -0200

    Adicionar novo professor

--- src/main/kotlin/br/com/ope/controller/PainelAdminProfessoresController.kt
+++ src/main/kotlin/br/com/ope/controller/PainelAdminProfessoresController.kt
@@ -2,11 +2,15 @@ package br.com.ope.controller
 
 import br.com.ope.model.Professor
 import br.com.ope.repository.ProfessorRepository
+import br.com.ope.vo.MensagemVO
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import javax.validation.Valid
 
 @Controller
 @RequestMapping("/painel/admin/professores")
@@ -27,6 +31,15 @@ class PainelAdminProfessoresController {
     fun novo(professor: Professor, bindingResult: BindingResult, model: Model) : String {
         popularForm(model, professor)
         return "/painel/admin/professores/novo"
+    }
+
+    @PostMapping("/novo")
+    fun novoSalvar(@Valid professor: Professor, bindingResult : BindingResult, model : Model, redirectAttributes: RedirectAttributes) : String {
+        if (bindingResult.hasErrors()) return novo(professor,bindingResult,model)
+        professor.ativo = true
+        professorRepository.save(professor)
+        redirectAttributes.addFlashAttribute("mensagem", MensagemVO("Professor salvo!","Sucesso!", MensagemVO.TipoMensagem.success ))
+        return "redirect:/painel/admin/professores"
     }
 
     private fun popularForm(model : Model, professor: Professor = Professor()) : Model {
