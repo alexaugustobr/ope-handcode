35e6f1d2c6e17e7cd74135cb63c876c33945e340
alexcologia33 <alexcologia33@gmail.com>
2019-03-22 16:02:05 -0300

    redirect logado

--- src/main/kotlin/br/com/ope/controller/LoginController.kt
+++ src/main/kotlin/br/com/ope/controller/LoginController.kt
@@ -1,6 +1,8 @@
 package br.com.ope.controller
 
+import br.com.ope.model.Usuario
 import br.com.ope.vo.MensagemVO
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,7 +14,11 @@ import javax.servlet.http.HttpServletRequest
 class LoginController {
 
     @RequestMapping("/login")
-    fun login(model: Model, @RequestParam(value = "error", required = false) error: Optional<String>, request : HttpServletRequest): String {
+    fun login(model: Model, @RequestParam(value = "error", required = false) error: Optional<String>, request : HttpServletRequest, @AuthenticationPrincipal usuario: Usuario?): String {
+        if (usuario != null) {
+            return "redirect:/painel/"+usuario.getPainelUrl()
+        }
+
         if (error.isPresent) {
             val attr = Optional.ofNullable(request.session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION"))
             if (attr.isPresent) {
