78cbc290e7bf2bad57bf944a2e1f232a2f7a562f
alexcologia33 <alexcologia33@gmail.com>
2019-04-04 01:02:04 -0300

    motivo em string

--- src/main/kotlin/br/com/ope/service/GrupoService.kt
+++ src/main/kotlin/br/com/ope/service/GrupoService.kt
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class GrupoService(val grupoRepository: GrupoRepository, val alunoRepository: AlunoRepository)  : DomainService() {
+class GrupoService(private val grupoRepository: GrupoRepository, private val alunoRepository: AlunoRepository)  : DomainService() {
 
     @Throws(EntidadeNaoEncontradaException::class, BusinessException::class)
-    fun removerAlunoDoGrupo(alunoId : UUID, grupoId : UUID) {
+    fun removerAlunoDoGrupo(alunoId : UUID, grupoId : UUID, motivo: String? = "") {
         val grupo = grupoRepository.findById(grupoId).orElseThrow { EntidadeNaoEncontradaException() }
         val aluno = alunoRepository.findById(alunoId).orElseThrow { EntidadeNaoEncontradaException() }
         grupo.removerAluno(aluno)
